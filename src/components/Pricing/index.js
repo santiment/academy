7ed075a7ed78b7a755ed9693a40140e5ddd6424c
DmitriVanGuard <dimavanguard@gmail.com>
2019-06-03 11:58:22 +0300

    Expanding price card

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -81,7 +81,7 @@ const prices = [
     ],
   },
   {
-    title: "Custom",
+    title: "Corporate",
     desc: "For organizations that need advanced data and support",
     price: "Custom",
     priceType: "",
@@ -107,6 +107,10 @@ function useToggle() {
   return [toggled, setToggle]
 }
 
+function toggleCardDetails({ currentTarget }) {
+  currentTarget.classList.toggle(styles.card_opened)
+}
+
 export default () => {
   const [toggled, setToggle] = useToggle()
   return (
@@ -121,23 +125,41 @@ export default () => {
           <div
             className={cx(styles.card, card.isPopular && styles.card_popular)}
             key={card.title}
+            onClick={toggleCardDetails}
           >
-            <h3 className={styles.card__title}>
-              {card.title}
-              {card.isPopular && (
-                <span className={styles.popular}>Popular</span>
-              )}
-            </h3>
-            <div className={styles.desc}>{card.desc}</div>
-            <div className={styles.price}>
-              {card.price}
-              <span className={styles.price__type}>{card.priceType}</span>
+            <div className={styles.card__top}>
+              <h3 className={styles.card__title}>
+                {card.title}
+                {card.isPopular && (
+                  <span className={styles.popular}>Popular</span>
+                )}
+              </h3>
+              <svg
+                width="15"
+                height="8"
+                viewBox="0 0 15 8"
+                className={styles.card__arrow}
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M14.313 0.754539C14.5867 1.06549 14.5564 1.5394 14.2455 1.81304L7.99546 7.31304C7.71218 7.56233 7.2878 7.56233 7.00452 7.31304L0.754519 1.81304C0.443563 1.5394 0.413314 1.06549 0.686955 0.754538C0.960596 0.443582 1.43451 0.413333 1.74546 0.686974L7.49999 5.75096L13.2545 0.686974C13.5655 0.413333 14.0394 0.443583 14.313 0.754539Z"
+                />
+              </svg>
             </div>
-            <div className={styles.discount}>{card.discount}</div>
-            <Link className={styles.link} to="/">
-              {card.link}
-            </Link>
-            <Features data={card.features} classes={styles} />
+            <div className={styles.desc}>{card.desc}</div>
+            <div className={styles.details}>
+              <div className={styles.price}>
+                {card.price}
+                <span className={styles.price__type}>{card.priceType}</span>
+              </div>
+              <div className={styles.discount}>{card.discount}</div>
+              <Link className={styles.link} to="/">
+                {card.link}
+              </Link>
+              <Features data={card.features} classes={styles} />
+            </div>
           </div>
         ))}
       </div>
