dac1935871a7c365387513a7ef87a36f76c06854
DmitriVanGuard <dimavanguard@gmail.com>
2019-06-10 12:45:56 +0300

    Gatsby header color

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,8 +33,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#5275FF`,
+        theme_color: `#5275FF`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
