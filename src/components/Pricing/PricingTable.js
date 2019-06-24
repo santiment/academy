c492a32ae4d52f43133ab3c4f26ed5b32ff7d554
DmitriVanGuard <dimavanguard@gmail.com>
2019-06-24 15:33:23 +0300

    Showing only month plans

--- src/components/Pricing/PricingTable.js
+++ src/components/Pricing/PricingTable.js
@@ -18,6 +18,8 @@ const PLANS_QUERY = gql`
       plans {
         id
         name
+        amount
+        interval
       }
     }
   }
@@ -59,83 +61,87 @@ export default ({ classes = {} }) => {
 
                 return (
                   <div className={styles.cards}>
-                    {neuro.plans.map(({ id, name }) => {
-                      const card = prices[name]
-                      const sameAsUserPlan = id === userPlan
-                      return (
-                        <div
-                          className={cx(
-                            styles.card,
-                            classes.card,
-                            card.isPopular && styles.card_popular,
-                          )}
-                          key={card.title}
-                          onClick={toggleCardDetails}
-                        >
-                          <div className={styles.card__top}>
-                            <h3 className={styles.card__title}>
-                              {card.title}
-                              {card.isPopular && (
-                                <span className={styles.popular}>Popular</span>
-                              )}
-                            </h3>
-                            <svg
-                              width='15'
-                              height='8'
-                              viewBox='0 0 15 8'
-                              className={styles.card__arrow}
-                              xmlns='http://www.w3.org/2000/svg'
-                            >
-                              <path
-                                fillRule='evenodd'
-                                clipRule='evenodd'
-                                d='M14.313 0.754539C14.5867 1.06549 14.5564 1.5394 14.2455 1.81304L7.99546 7.31304C7.71218 7.56233 7.2878 7.56233 7.00452 7.31304L0.754519 1.81304C0.443563 1.5394 0.413314 1.06549 0.686955 0.754538C0.960596 0.443582 1.43451 0.413333 1.74546 0.686974L7.49999 5.75096L13.2545 0.686974C13.5655 0.413333 14.0394 0.443583 14.313 0.754539Z'
-                              />
-                            </svg>
-                          </div>
-                          <div className={styles.desc}>{card.desc}</div>
-                          <div className={styles.details}>
-                            <div className={styles.price}>
-                              {card.price}
-                              <span className={styles.price__type}>
-                                {card.priceType}
-                              </span>
-                            </div>
-                            <div className={styles.discount}>
-                              {card.discount}
-                            </div>
-                            {card.title === 'Free' ||
-                            card.title === 'Enterprise' ? (
-                              <Button
-                                as={Link}
-                                to='/account'
-                                fluid
-                                accent='blue'
-                                border
-                                className={styles.link}
-                              >
-                                {card.link}
-                              </Button>
-                            ) : (
-                              <PaymentDialog
-                                title={card.title}
-                                label={
-                                  sameAsUserPlan
-                                    ? 'Your current plan'
-                                    : card.link
-                                }
-                                src={card.pipedrive}
-                                price={card.price}
-                                planId={id}
-                                disabled={sameAsUserPlan}
-                                isLoggedIn={currentUser}
-                              />
+                    {neuro.plans
+                      .filter(({ interval }) => interval === 'month')
+                      .map(({ id, name }) => {
+                        const card = prices[name]
+                        const sameAsUserPlan = id === userPlan
+                        return (
+                          <div
+                            className={cx(
+                              styles.card,
+                              classes.card,
+                              card.isPopular && styles.card_popular,
                             )}
-                            <Features data={card.features} classes={styles} />
+                            key={card.title}
+                            onClick={toggleCardDetails}
+                          >
+                            <div className={styles.card__top}>
+                              <h3 className={styles.card__title}>
+                                {card.title}
+                                {card.isPopular && (
+                                  <span className={styles.popular}>
+                                    Popular
+                                  </span>
+                                )}
+                              </h3>
+                              <svg
+                                width='15'
+                                height='8'
+                                viewBox='0 0 15 8'
+                                className={styles.card__arrow}
+                                xmlns='http://www.w3.org/2000/svg'
+                              >
+                                <path
+                                  fillRule='evenodd'
+                                  clipRule='evenodd'
+                                  d='M14.313 0.754539C14.5867 1.06549 14.5564 1.5394 14.2455 1.81304L7.99546 7.31304C7.71218 7.56233 7.2878 7.56233 7.00452 7.31304L0.754519 1.81304C0.443563 1.5394 0.413314 1.06549 0.686955 0.754538C0.960596 0.443582 1.43451 0.413333 1.74546 0.686974L7.49999 5.75096L13.2545 0.686974C13.5655 0.413333 14.0394 0.443583 14.313 0.754539Z'
+                                />
+                              </svg>
+                            </div>
+                            <div className={styles.desc}>{card.desc}</div>
+                            <div className={styles.details}>
+                              <div className={styles.price}>
+                                {card.price}
+                                <span className={styles.price__type}>
+                                  {card.priceType}
+                                </span>
+                              </div>
+                              <div className={styles.discount}>
+                                {card.discount}
+                              </div>
+                              {card.title === 'Free' ||
+                              card.title === 'Enterprise' ? (
+                                <Button
+                                  as={Link}
+                                  to='/account'
+                                  fluid
+                                  accent='blue'
+                                  border
+                                  className={styles.link}
+                                >
+                                  {card.link}
+                                </Button>
+                              ) : (
+                                <PaymentDialog
+                                  title={card.title}
+                                  label={
+                                    sameAsUserPlan
+                                      ? 'Your current plan'
+                                      : card.link
+                                  }
+                                  src={card.pipedrive}
+                                  price={card.price}
+                                  planId={id}
+                                  disabled={sameAsUserPlan}
+                                  isLoggedIn={currentUser}
+                                />
+                              )}
+                              <Features data={card.features} classes={styles} />
+                            </div>
                           </div>
-                        </div>
-                      )
-                    })}
+                        )
+                      })}
                   </div>
                 )
               }}
