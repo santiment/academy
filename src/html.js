23c994b08702150afd01d875f11d60e0dc783a5e
DmitriVanGuard <dimavanguard@gmail.com>
2019-07-25 21:52:58 +0300

    hotjar script

--- src/html.js
+++ src/html.js
@@ -9,6 +9,21 @@ const intercomScript = (
   />
 )
 
+const hotjarScript = (
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1403198,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+    }}
+  />
+)
+
 export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
@@ -22,6 +37,7 @@ export default function HTML(props) {
         {props.headComponents}
         <script src='https://js.stripe.com/v3/' />
         {intercomScript}
+        {hotjarScript}
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
