38ac2fe886f52f743104d76969f7e9158b99a7c9
haritonasty <a.kharitonovaspb@gmail.com>
2019-08-05 11:06:03 +0300

    removed unnecessary

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Neuro - Crypto’s Most Comprehensive API`,
     description: `Raw data access to on-chain, social media, development activity and pricing information for 1000+ crypto assets. Unlimited historical and real time data. Backtest strategies, build custom signals, DIY dashboards and much more with the most all-inclusive crypto API on the market.`,
     author: `Santiment Team`,
-    siteUrl: 'https://neuro-stage.santiment.net'
+    siteUrl: 'https://neuro.santiment.net'
   },
   plugins: [
     {
