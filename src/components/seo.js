2761ed635ed535d9ed3feb71e4fd49f7a158b92f
haritonasty <a.kharitonovaspb@gmail.com>
2019-08-05 10:48:38 +0300

    added siteUrl

--- src/components/seo.js
+++ src/components/seo.js
@@ -20,6 +20,7 @@ function SEO({ description, lang, meta, title }) {
             title
             description
             author
+            siteUrl
           }
         }
       }
@@ -27,6 +28,7 @@ function SEO({ description, lang, meta, title }) {
   )
 
   const metaDescription = description || site.siteMetadata.description
+  const imageUrl = `${site.siteMetadata.siteUrl}${neuro}`
 
   return (
     <Helmet
@@ -41,7 +43,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           property: `og:image`,
-          content: neuro,
+          content: imageUrl,
         },
         {
           property: `og:title`,
@@ -73,7 +75,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           name: `twitter:image`,
-          content: neuro,
+          content: imageUrl,
         }
       ].concat(meta)}
     />
