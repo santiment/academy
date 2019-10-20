7120cfd3c8f4c8fd380baee70cf30ffee1e92c8b
haritonasty <a.kharitonovaspb@gmail.com>
2019-10-20 21:32:04 +0300

    more categories and blocks

--- src/components/Block/Block.js
+++ src/components/Block/Block.js
@@ -1,19 +1,23 @@
 import React from "react"
+import cx from 'classnames'
 import Icon from "@santiment-network/ui/Icon"
 import {Link} from "gatsby"
 import {titleToSlug} from '../../utils/docs'
 import styles from "./Block.module.scss"
 
-const Block = ({title, articles, category}) => (
-	<Link to={`/${titleToSlug(category)}/${titleToSlug(title)}`} className={styles.block}>
+const Block = ({title, articles, category}) => {
+	const {length} = articles
+	return (
+	<Link to={length ? `/${titleToSlug(category)}/${titleToSlug(title)}` : ''} className={cx(styles.block, !length && styles.disable)}>
 		<div className={styles.icon}>
 		<Icon type='words-list' />
 		</div>
 		<div>
 		<h4 className={styles.title}>{title}</h4>
-		<p className={styles.length}>{articles.length} guides</p>
+		<p className={styles.length}>{length ? length > 1 ? `${length} guides` : '1 guide' : 'Guides in work'}</p>
 		</div>
 	</Link>
 )
+}
 
 export default Block
--- src/utils/docs.js
+++ src/utils/docs.js
@@ -1,48 +1,108 @@
-export const BLOCKS = {
-	Financial: {
+export const METRICS = {
+	financial: {
 		title: 'Financial',
 		articles: ['Mean coin age']
 	},
-	Development: {
+	development: {
 		title: 'Development',
 		articles: []
 	},
-	'On-chain': {
+	onchain: {
 		title: 'On-chain',
 		articles: ['Daily Active Addresses']
 	},
-	'Social': {
+	social: {
 		title: 'Social/Sentiment',
 		articles: ['sentiment-metrics', 'Social volume metrics', 'Social data']
 	}
+}
+
+export const SANBASE = {
+	about: {
+		title: 'About Sanbase',
+		articles: []
+	},
+	myAccount: {
+		title: 'My account',
+		articles: []
+	},
+	pricingBilling: {
+		title: 'Pricing & Billing',
+		articles: []
+	},
+}
+
+export const SHEETS = {
+	about: {
+		title: 'About Sheets',
+		articles: []
+	},
+	interface: {
+		title: 'Interface',
+		articles: []
+	},
+	functions: {
+		title: 'Functions',
+		articles: []
+	},
+	integrations: {
+		title: 'Integrations',
+		articles: []
+	},
+}
+
+export const GRAPHS = {
+	about: {
+		title: 'About Graphs',
+		articles: []
+	},
+	howWorks: {
+		title: 'How it works',
+		articles: []
+	},
+}
+
+export const NEURO = {
+	about: {
+		title: 'About Neuro API',
+		articles: []
+	},
+	install: {
+		title: 'Install guide',
+		articles: []
+	},
+	howWorks: {
+		title: 'How API works',
+		articles: []
+	},
 }
 
 export const CATEGORIES = [
 	{
 		title: 'Metrics',
 		description: 'Santiment brings a comprehensive set of metrics together in one place',
-		blocks: [BLOCKS.Financial, BLOCKS['Social'], BLOCKS.Development, BLOCKS['On-chain']]
+		blocks: [METRICS.financial, METRICS.social, METRICS.development, METRICS.onchain]
+	},
+	{
+		title: 'Sanbase',
+		description: 'Platform for in-depth coin analysis, low-latency signals, asset watchlists',
+		blocks: [SANBASE.about, SANBASE.myAccount, SANBASE.pricingBilling]
+	},
+	{
+		title: 'Sansheets',
+		description: 'Google Spreadsheet plugin for Santiment data',
+		blocks: [SHEETS.about, SHEETS.interface, SHEETS.functions, SHEETS.integrations]
+	},
+	{
+		title: 'Sangraphs',
+		description: 'Advanced on-chain, social and development metrics and charts for 700+ coins',
+		blocks: [GRAPHS.about, GRAPHS.howWorks]
+	},
+	{
+		title: 'Neuro API',
+		description: 'The most comprehensive crypto API on the market',
+		blocks: [NEURO.about, NEURO.install, NEURO.howWorks]
 	}
-	// {
-	// 	title: 'Sanbase',
-	// 	description: 'Platform for in-depth coin analysis, low-latency signals, asset watchlists',
-	// 	blocks: [BLOCKS.aboutSanbase, BLOCKS.metrics]
-	// },
-	// {
-	// 	title: 'Sansheets',
-	// 	description: 'Google Spreadsheet plugin for Santiment data',
-	// 	blocks: []
-	// },
-	// {
-	// 	title: 'Sangraphs',
-	// 	description: 'Advanced on-chain, social and development metrics and charts for 700+ coins',
-	// 	blocks: []
-	// },
-	// {
-	// 	title: 'Neuro API',
-	// 	description: 'The most comprehensive crypto API on the market',
-	// 	blocks: []
-	// }
 ]
 
 export const titleToSlug = title => title.toLowerCase().split(' ').join('-').split('/').join('-')
