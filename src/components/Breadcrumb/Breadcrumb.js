be221687157fc2fef9becf2e31fc474ba6d7a999
haritonasty <a.kharitonovaspb@gmail.com>
2019-11-20 00:55:23 +0300

    control phone breadcrumbs from css

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -1,43 +1,29 @@
 import React from "react"
-import withSizes from "react-sizes"
 import cx from "classnames"
 import {Link} from "gatsby"
-import { mapSizesToProps } from "../../utils/sizes"
 import Icon from "@santiment-network/ui/Icon"
 import styles from "./Breadcrumb.module.scss"
 
-const Breadcrumb = ({crumbs,crumbLabel, className, isPhone = false}) => {
+const Breadcrumb = ({crumbs,crumbLabel, className}) => {
 	const crumbsWithoutLast = crumbs.map(({pathname, crumbLabel}) => ({crumbLabel: crumbLabel.charAt(0).toUpperCase() + crumbLabel.slice(1), pathname}))
 	crumbsWithoutLast.pop()
-	const previous = crumbsWithoutLast[crumbsWithoutLast.length - 1]
-	return !isPhone ? (
-<nav className={cx(styles.wrapper, className)}>
-		<ul className={styles.list}>
-			{crumbsWithoutLast.map(({pathname, crumbLabel}) => (
-			<li key={pathname} className={styles.title}>
-				<Link to={pathname}>
-					{crumbLabel}
-				</Link>
-				<Icon type='arrow-right' className={styles.icon} />
-			</li>
-			))}
-			<li className={styles.last}>
-				{crumbLabel}
-			</li>
-		</ul>
-	</nav>
-		) : (
-				<div className={styles.wrapper}>
-			<div className={styles.title}>
-				<Link to={previous.pathname}>
-					<Icon type='arrow-left' className={styles.icon} />
-					{previous.crumbLabel}
-				</Link>
-			</div>
-		</div>
-	)
-}
+	return (<nav className={cx(styles.wrapper, className)}>
+				<ul className={styles.list}>
+					{crumbsWithoutLast.map(({pathname, crumbLabel}) => (
+					<li key={pathname} className={styles.title}>
+						<Link to={pathname}>
+							{crumbLabel}
+						</Link>
+						<Icon type='arrow-right' className={styles.icon} />
+					</li>
+					))}
+					<li className={styles.last}>
+						{crumbLabel}
+					</li>
+				</ul>
+			</nav>
+)}
 
-export default withSizes(mapSizesToProps)(Breadcrumb)
+export default Breadcrumb
 
 
