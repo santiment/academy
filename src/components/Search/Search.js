01fc71b62bdce1417df25d7cfe6471b42c7aa9b8
haritonasty <a.kharitonovaspb@gmail.com>
2019-12-17 14:06:41 +0300

    upd main page

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,20 +1,48 @@
-import React from "react"
-import Input from '@santiment-network/ui/Search/Search'
+import React, { useRef } from "react"
+import { InputWithIcon as Input } from "@santiment-network/ui/Input"
 import styles from "./Search.module.scss"
 
-const Search = () => (
-	<div className={styles.wrapper}>
-		<h3 className={styles.heading}>Santiment Academy</h3>
-		<Input id="search" iconClassName={styles.icon} inputClassName={styles.input} />
-		<div className={styles.suggestions}>
-			<span className={styles.group}>Popular searches:</span>
-			<span className={styles.word}>Tokens</span>
-			<span className={styles.word}>Sansheets</span>
-			<span className={styles.word}>Metrics</span>
-			<span className={styles.word}>Api key</span>
-			<span className={styles.word}>Social data</span>
-		</div>
-	</div>
-)
+const SUGGESTIONS = [
+  "Tokens",
+  "Sansheets",
+  "Metrics",
+  "API key",
+  " Social data",
+]
+
+const Search = () => {
+  const inputEl = useRef(null)
+
+  const onSuggestionClick = value => {
+    if (inputEl) {
+    	inputEl.current.value = value
+    	// inputEl.current.focus()
+    }
+  }
+
+
+  return (
+    <div className={styles.wrapper}>
+      <h3 className={styles.heading}>Santiment Academy</h3>
+      <Input
+        id='search'
+        placeholder='Search docs, articles, video tutorials...'
+        icon='search'
+        forwardedRef={inputEl}
+        iconPosition='left'
+        iconClassName={styles.icon}
+        inputClassName={styles.input}
+      />
+      <div className={styles.suggestions}>
+        <span className={styles.group}>Popular searches:</span>
+        {SUGGESTIONS.map(item =>
+          <span key={item} className={styles.word} onClick={() => onSuggestionClick(item)}>
+            {item}
+          </span>
+        )}
+      </div>
+    </div>
+  )
+}
 
 export default Search
