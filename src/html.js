344ed62e1bfa0170b0e4528c9aa3256141179ee8
Yura Zatsepin <yura.zatsepin@gmail.com>
2020-01-24 14:46:42 +0500

    Add new GA Property

--- src/html.js
+++ src/html.js
@@ -41,10 +41,10 @@ const gtagScript = (
   <script
     dangerouslySetInnerHTML={{
       __html: `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-100571693-14');`,
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-100571693-14');
+  gtag('config', 'UA-100571693-1');`,
     }}
   />
 )
