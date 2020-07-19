525c59462e28bfb94b422b7d2ddf39afbd469164
haritonasty <a.kharitonovaspb@gmail.com>
2020-07-19 12:02:07 +0300

    suggestions with links

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,26 @@
 import React, { useRef, useEffect } from "react"
 import cx from 'classnames'
+import { Link } from 'gatsby'
 import { InputWithIcon as Input } from "@santiment-network/ui/Input"
 import { isSSR } from '../../utils/utils'
 import styles from "./Search.module.scss"
 
-const SUGGESTIONS = ["Trending words", "Social volume", "MVRV", "Metrics"]
+const SUGGESTIONS = [{
+  label: "Trending words",
+  link: "/metrics/emerging-trends/"
+},
+{
+  label: "Social volume",
+  link: "/metrics/social-volume/"
+},
+{
+  label: "MVRV",
+  link: "/metrics/mvrv/"
+},
+{
+  label: "Metrics",
+  link: "/metrics/"
+}]
 
 const Search = ({ small }) => {
   useEffect(() => {
@@ -19,11 +35,11 @@ const Search = ({ small }) => {
 
   const inputEl = useRef(null)
 
-  const onSuggestionClick = value => {
-    if (inputEl) {
-    	inputEl.current.value = value
-    }
-  }
+  // const onSuggestionClick = value => {
+  //   if (inputEl) {
+  //   	inputEl.current.value = value
+  //   }
+  // }
 
 
   return small ? (
@@ -52,10 +68,10 @@ const Search = ({ small }) => {
       />
       <div className={styles.suggestions}>
         <span className={styles.group}>Popular searches:</span>
-        {SUGGESTIONS.map(item =>
-          <span key={item} className={styles.word} onClick={() => onSuggestionClick(item)}>
-            {item}
-          </span>
+        {SUGGESTIONS.map(({link, label}) =>
+          <Link to={link} key={label} className={styles.word}>
+            {label}
+          </Link>
         )}
       </div>
     </div>
