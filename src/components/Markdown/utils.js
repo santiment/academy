3eb48a263ac0dd7a8658a7fd864c183a9c767f26
haritonasty <a.kharitonovaspb@gmail.com>
2020-12-07 19:36:05 +0300

    allow numbers in headings

--- src/components/Markdown/utils.js
+++ src/components/Markdown/utils.js
@@ -11,7 +11,7 @@ export const sluggify = text => text
     .toString().toLowerCase()
     .replace(/\s+/g, '-') // Replace spaces with -
 	.replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^A-Za-z_]+/g, '-') // Remove all non-word chars
+    .replace(/[^A-Za-z_0-9]+/g, '-') // Remove all non-word chars
     .replace(/-+/g, '-') // Replace multiple - with single -
     .replace(/^-+/, '') // Trim - from start of text
     .replace(/-+$/, '');
