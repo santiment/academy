24b381145f2884216a2065ba53f6957087d4e621
haritonasty <a.kharitonovaspb@gmail.com>
2021-05-13 01:24:08 +0300

    upd nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,49 +1,20 @@
-import React, { useRef } from "react"
+import React from "react"
 import withSizes from "react-sizes"
 import cx from "classnames"
-import { Link } from "gatsby"
-import Icon from "@santiment-network/ui/Icon"
 import { mapSizesToProps } from "../../utils/sizes"
-import Dropdown from "../Dropdown/Dropdown"
 import Search from '../../components/Search/Search'
-import dropdownStyles from "../Dropdown/Dropdown.module.scss"
-import styles from "./Header.module.scss"
+import Logo from "../SantimentProductsTooltip/Logo"
 import SantimentProductsTooltip from "../SantimentProductsTooltip/SantimentProductsTooltip"
+import styles from "./Header.module.scss"
 
-const Header = ({ isDesktop, isTablet, isShowSidebar, className }) => {
-  const toggle = useRef(null)
-
-  return (
+const Header = ({ isShowSidebar, className }) => (
     <header className={cx(styles.wrapper, className)}>
       <div className={cx(styles.header, 'container')}>
+        <Logo />
         <SantimentProductsTooltip />
         {isShowSidebar && <Search small />}
-      {false && (<>
-      <label htmlFor='hamburger'>
-        <Icon type='hamburger' />
-      </label>
-      <input id='hamburger' type='checkbox' ref={toggle} />
-      <nav className={styles.nav}>
-        <label htmlFor='hamburger' className={styles.close}>
-          <Icon type='close' />
-        </label>
-        {isDesktop || isTablet ? (
-          <Dropdown
-            title={<Icon type='profile' />}
-            isDesktop={isDesktop}
-          >
-            <div className={dropdownStyles.list}>
-              <Link to="/login">Log in</Link>
-            </div>
-          </Dropdown>
-        ) : (
-          <Link to="/login">Log in</Link>
-        )}
-      </nav>
-    </>)}
-    </div>
+      </div>
     </header>
-  )
-}
+)
 
 export default withSizes(mapSizesToProps)(Header)
