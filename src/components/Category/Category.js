87a76bcfebab883d727dfd4ad2e68e41189db7e3
haritonasty <a.kharitonovaspb@gmail.com>
2021-05-21 18:01:38 +0300

    upd guides icon colors

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,15 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import cx from 'classnames'
 import { titleToSlug } from "../../utils/docs"
 import styles from "./Category.module.scss"
 
 const Category = ({ wide, title, description, icon: Icon }) => {
+  const [isHover, setIsHover] = useState(false)
   const slug = titleToSlug(title)
+
+  const setHover = () => {
+    if (!isHover) {
+      setIsHover(true)
+    }
+  }
+
+  const removeHover = () => {
+    if (isHover) {
+      setIsHover(false)
+    }
+  }
+
   return (
-    <Link to={`/${slug}/`} className={cx(styles.block, wide && styles.wide)}>
-      <div className={cx(styles.icon)}>
-        <Icon />
+    <Link
+      to={`/${slug}/`}
+      className={cx(styles.block, wide && styles.wide)}
+      onTouchStart={setHover}
+      onMouseEnter={setHover}
+      onTouchEnd={removeHover}
+      onTouchCancel={removeHover}
+      onMouseLeave={removeHover}
+    >
+      <div className={styles.icon}>
+        <Icon withColor={isHover} />
       </div>
       <div>
         <h4 className={styles.title}>{title}</h4>
