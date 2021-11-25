5d73e2bc05ebb18f507fb1a7877fba9e4932d50e
SaeidR <saeidr@evenset.com>
2021-11-25 04:45:31 -0500

    moment removed

--- src/components/ArticleInfo/ArticleInfo.js
+++ src/components/ArticleInfo/ArticleInfo.js
@@ -1,6 +1,6 @@
 import React from "react"
 import cx from 'classnames'
-import moment from 'moment'
+import {getDateFormats} from '../../utils/dates'
 import {PEOPLE} from './../../docs/people'
 import logo from '../../images/logos/logo-sanbase.svg'
 import styles from "./ArticleInfo.module.scss"
@@ -10,6 +10,7 @@ const DEFAULT_NAME = 'Santiment Team'
 const ArticleInfo = ({title, author, date}) => {
 	const name = author || DEFAULT_NAME
 	const {description, imageUrl} = PEOPLE[name.toLowerCase()] || {}
+	const { DD, MMM, YYYY } = getDateFormats(new Date(date))
 	return (
 	<section className={styles.block}>
 		<h3 className={styles.title}>{title}</h3>
@@ -20,7 +21,7 @@ const ArticleInfo = ({title, author, date}) => {
 			<div>
 				<h4 className={styles.author}>{name}</h4>
 				{date ? (
-						<span className={styles.description}>{moment(date).format("MMM DD, YYYY")}</span>
+						<span className={styles.description}>{`${MMM} ${DD}, ${YYYY}`}</span>
 					) : (
 						<span className={styles.description}>{description}</span>
 					)}
