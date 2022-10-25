ddef7cbbeeb066fa82102d7dc2c03e5f0f811a69
SaeidR <saeid.r@santiment.net>
2022-10-25 05:07:19 -0400

    comment removed

--- src/components/ArticleHeadings/hooks.js
+++ src/components/ArticleHeadings/hooks.js
@@ -8,7 +8,6 @@ function getElements(tableOfContents) {
   )
   const ul = parsedHtml.getElementsByTagName('ul')
   const pCount = parsedHtml.getElementsByTagName('p').length
-  // if (pCount === 1 && ul.length === 2) {
   if (ul.length - pCount === 1) {
     return [ul[1]]
   }
