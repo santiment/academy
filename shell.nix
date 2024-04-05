bac0c91be079afe1b16573ef70dab77a83138d39
tsanko@gmail.com <tsanko@gmail.com>
2024-04-05 17:33:04 +0300

    Add Youtube video for top Santiment Metrics

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ pkgs.mkShell {
     pkgs.nodejs
     pkgs.yarn
   ];
+  NODE_OPTIONS="--openssl-legacy-provider";
 
   shellHook = ''
     echo "Ready to code! You can start the app with 'yarn start'."
