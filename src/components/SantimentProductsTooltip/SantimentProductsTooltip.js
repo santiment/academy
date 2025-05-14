abe504cc85b41907c9a04dc692267bcb06a576d8
Serafim <serafim.g@santiment.net>
2025-05-14 14:32:20 +0400

    feat: products nav links update

--- src/components/SantimentProductsTooltip/SantimentProductsTooltip.js
+++ src/components/SantimentProductsTooltip/SantimentProductsTooltip.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
-import cx from 'classnames'
-import Tooltip from '@santiment-network/ui/Tooltip'
-import ProductItem from './Product'
-import { BUSINESS_PRODUCTS, CHAIN_PRODUCTS } from './Products'
-import { ProductsTrigger } from './Trigger'
-import styles from './SantimentProductsTooltip.module.scss'
+import React, { useState } from "react"
+import cx from "classnames"
+import Tooltip from "@santiment-network/ui/Tooltip"
+import ProductItem from "./Product"
+import {
+  BUSINESS_PRODUCTS,
+  CHAIN_PRODUCTS,
+  INITIATIVES_PRODUCTS,
+} from "./Products"
+import { ProductsTrigger } from "./Trigger"
+import styles from "./SantimentProductsTooltip.module.scss"
 
 const SantimentProductsTooltip = ({ className }) => {
   const [isOpen, setOpen] = useState(false)
@@ -27,15 +31,29 @@ const SantimentProductsTooltip = ({ className }) => {
     >
       <div className={styles.container}>
         <div className={styles.block}>
-          <h3 className={styles.title}>SAN business</h3>
-          <div className={styles.products}>
-            {BUSINESS_PRODUCTS.map((item, index) => (
-              <ProductItem
-                key={index}
-                product={item}
-                className={styles.product}
-              />
-            ))}
+          <div class={styles.business}>
+            <h3 className={styles.title}>SAN business</h3>
+            <div className={styles.products}>
+              {BUSINESS_PRODUCTS.map((item, index) => (
+                <ProductItem
+                  key={index}
+                  product={item}
+                  className={styles.product}
+                />
+              ))}
+            </div>
+          </div>
+          <div>
+            <h3 className={styles.title}>Initiatives</h3>
+            <div className={styles.products}>
+              {INITIATIVES_PRODUCTS.map((item, index) => (
+                <ProductItem
+                  key={index}
+                  product={item}
+                  className={styles.product}
+                />
+              ))}
+            </div>
           </div>
         </div>
         <div className={styles.block}>
