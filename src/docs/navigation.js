fab02bd5c376132b6bc9dae65741b54dad663d64
filips121 <filip.s@santiment.net>
2025-11-26 18:05:01 +0100

    Add Thermocap and MCTC article

--- src/docs/navigation.js
+++ src/docs/navigation.js
@@ -350,6 +350,7 @@ export const REFERENCES = [
       'Stock To Flow',
       'Supply Distribution',
       'Supply On Or Outside Exchanges',
+      'Thermocap',
       'Top Holders',
       'Top Social Gainers Losers',
       'Total Supply',
