58c67ced7451f223f7fb60ebbcbf17beebfd4ed8
Serafim <serafim@Serafims-MacBook-Pro.local>
2026-02-24 20:18:23 -0300

    feat: updated flags

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,13 @@ const viteBase = await createAstroConfig()
 const viteConfig = mergeConfig(viteBase, {
   define: {
     __SVELTEKIT_APP_VERSION_POLL_INTERVAL__: '0',
+    __SVELTEKIT_PATHS_BASE__: '""',
+    __SVELTEKIT_ASSETS__: '""',
+    __SVELTEKIT_APP_VERSION__: '""',
+    __SVELTEKIT_PATHS_ASSETS__: '""',
+    __SVELTEKIT_PAYLOAD__: '""',
+    __SVELTEKIT_APP_DIR__: '""',
+    __SVELTEKIT_PATHS_RELATIVE__: '""',
   },
   alias: {
     $components: path.resolve(__dirname, './src/components'),
